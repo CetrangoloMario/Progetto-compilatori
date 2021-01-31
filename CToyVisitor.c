76fca1dad85a273b835ca3c4cf98b93870827808
Mario Cetrangolo <55892147+CetrangoloMario@users.noreply.github.com>
2021-01-31 12:18:00 +0100

    Ricontrollati .c, problema intellijea aggiornamento cancellato configurazioni

--- CToyVisitor.c
+++ CToyVisitor.c
@@ -2,163 +2,52 @@
 #include <stdbool.h>
 #include <string.h>
 
+	char nome[500]  = "Michele";
 
-float calcolaProdotto (float n1, float n2){
-	int i = 0;
-float temp = 0.0;
-
-		while( i < n2 ){
-			temp = temp + n1;
-		i = i + 1;
-	}
-	i
-	 return temp;
-
-}
-
-
-float calcolaEsponente (float n1, float n2){
-	int i = 0;
-float temp = 0.0;
-float num2 = 1;
-
-		while( ii < n2 ){
-			temp = temp + 	calcolaProdotto (n1, num2);
-	}
-	ii
-	 return temp;
-
-}
-
-
-float succFibonacci (float n1){
-	float fib = 0.0, fib1, fib2, i;
-
+ void writeNewLines (int n){
 	
-	if (iin1 == 0 || n1 == 1) { 
-			fib = 1;
-
+		while( n > 0 ){
+				printf("%s " , "\n");
+		n = n - 1;
 	}
-	 else {
-			fib1 = 1;
-		fib2 = 2;
-		i = 1;
-			while( i < n1 ){
-			fib = fib1 + fib2;
-		i = i + 1;
-		fib2 = fib1;
-		fib1 = fib;
-	}
-	i
-	}
-
-	 return fib;
-
+	
 }
 
 
-float calcola (int n, float num1, float num2){
-	float result = 0.0;
+ void multAddDiff (int *int_parametri0, int *int_parametri1, int *int_parametri2){
+	int primo, secondo, mul, add, diff;
 
-	
-	if (in == 1) { 
-			result = num1 + num2;
-
-	}
-
-	else { if(n == 5) {
-			result = 	succFibonacci (num1);
-	 }
-
-	else { if(n == 4) {
-			
-	if (num2 < 0) { 
-				printf("%s " , "Numero inserito non positivo non si può fare la elevazione");
-		result = 0.0;
-
-	}
-	 else {
-			result = 	calcolaEsponente (num1, num2);
-
-	}
-	 }
-
-	else { if(n == 3) {
-			
-	if (num1 < 1) { 
-				printf("%s " , "Numero inserito non positivo non si può fare la divisione");
-		result = 0.0;
-
-	}
-
-	else { if(num2 < 1) {
-				printf("%s " , "Numero inserito non positivo non si può fare la divisione");
-		result = 0.0;
-	 }
-	 else {
-			result = num1 / num2;
-
-	}
-
-	}
-	 }
-
-	else { if(n == 2) {
-			result = 	calcolaProdotto (num1, num2);
-	 }
-
-				}
-
-			}
-
-		}
-
-	}
-
-	 return result;
+		printf("%s " , "Inserire il primo argomento:\n");
+		scanf("%d", &primo);
+		printf("%s " , "Inserire il secondo argomento:\n");
+		scanf("%d", &secondo);
+	mul = primo * secondo;
+	add = primo + secondo;
+	diff = primo - secondo;
+	*int_parametri0 = mul;
+	*int_parametri1 = add;
+	*int_parametri2 = diff;
 
 }
 
 
  int main (){
-	int operazione, n = 1;
-float num1, num2, result;
-bool flag = true;
+	int a, b, c = 0;
 
-		while( flag ){
-			
-	if (n != 0) { 
-				printf("%s " , "Scegli operazione aritmetrica: \n");
-			printf("%s " , "\n 1 = somma, 2 = moltiplicazione, 3 = divisione, 4 = elevamento a potenza, 5 = successione di Fibonacci\n\n");
-			scanf("%d", &operazione);
-		
-	if (operazione >= 1 && operazioneoperazione < 5) { 
-				printf("%s " , "\n Inserisci il primo numero: \n");
-			scanf("%f", &num1);
-			printf("%s " , "\n Inserisci il secondo numero: \n");
-			scanf("%f", &num2);
-			printf("%s %f " , "Risultato è: ", 	calcola (operazione, num1, num2));
-
-	}
-
-	else { if(operazione == 5) {
-				printf("%s " , "\n Inserisci il primo numero: \n");
-			scanf("%f", &num1);
-			printf("%s %f " , "Risultato è: ", 	calcola (operazione, num1, num1));
-	 }
-	 else {
-				printf("%s " , "\tNumero inserito non valido\n");
-		n = 0;
-		flag = false;
-
-	}
-
-	}
-			printf("%s " , "\t\t FINE \n\n");
-
-	}
-	}
 	
+	int int_return0;
+
+	int int_return1;
+
+	int int_return2;
+	multAddDiff ( &int_return0,  &int_return1,  &int_return2);
+	a = int_return0;
+	b = int_return1;
+	c = int_return2;
+		printf("%s %s " , "Ciao ", nome);
+		writeNewLines (2);
+		printf("%s %d %s %d %s %d %s " , "I tuoi valori sono:\n", a, " per la moltiplicazione\n", b, " per la somma, e \n", c, " per la differenza");
+
 	 return 0;
 }
 
